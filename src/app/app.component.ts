989db81933191c9724b166eaf5deb994eb318e55
Speedy09 <sniadek97@gmail.com>
2020-03-24 13:46:58 +0100

    creating example tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list'
+  title = 'to-do-list';
+
+  taskName: string;
+  taskDate: string;
+  editMode = false;
+
+  tasks: Task[] = [
+    {
+      name: 'Fitness',
+      deadLine: new Date().toLocaleDateString(),
+      done: false
+    },
+    {
+      name: 'Housekeeping',
+      deadLine: new Date().toLocaleDateString(),
+      done: false
+    }
+  ]
 }
